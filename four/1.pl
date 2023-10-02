3610c6ccad4017a01f81227e12f5896c0735207c
honghuizhao <114247925+hong-hui-zhao@users.noreply.github.com>
2023-10-02 19:11:54 +0800

    Add files via upload

---
+++ four/1.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+
+# 生成随机的 DNA 序列
+sub generate_dna_sequence {
+    $length = shift;
+    @bases = ('A', 'C', 'G', 'T');
+    $sequence = '';
+    for ($i = 0; $i < $length; $i++) {
+        $random_base = $bases[rand @bases];
+        $sequence .= $random_base;
+    }
+    return $sequence;
+}
+
+# 生成两条长度为50bp的 DNA 序列
+$sequence1 = generate_dna_sequence(50);
+$sequence2 = generate_dna_sequence(50);
+
+print "Sequence 1: $sequence1\n";
+print "Sequence 2: $sequence2\n";
+
+# 计算匹配的碱基总数
+$match_count = 0;
+for ($i = 0; $i < length($sequence1); $i++) {
+    $base1 = substr($sequence1, $i, 1);
+    $base2 = substr($sequence2, $i, 1);
+    if ($base1 eq $base2) {
+        $match_count++;
+    }
+}
+
+print "Match count : $match_count\n";
---
+++ four/2.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+use utf8;
+
+binmode(STDOUT,":encoding(gbk)");
+
+ %codon_table = (
+    'TTT' => 'F', 'TCT' => 'S', 'TAT' => 'Y', 'TGT' => 'C',
+    'TTC' => 'F', 'TCC' => 'S', 'TAC' => 'Y', 'TGC' => 'C',
+    'TTA' => 'L', 'TCA' => 'S', 'TAA' => '.', 'TGA' => '.',
+    'TTG' => 'L', 'TCG' => 'S', 'TAG' => '.', 'TGG' => 'W',
+    'CTT' => 'L', 'CCT' => 'P', 'CAT' => 'H', 'CGT' => 'R',
+    'CTC' => 'L', 'CCC' => 'P', 'CAC' => 'H', 'CGC' => 'R',
+    'CTA' => 'L', 'CCA' => 'P', 'CAA' => 'Q', 'CGA' => 'R',
+    'CTG' => 'L', 'CCG' => 'P', 'CAG' => 'Q', 'CGG' => 'R',
+    'ATT' => 'I', 'ACT' => 'T', 'AAT' => 'N', 'AGT' => 'S',
+    'ATC' => 'I', 'ACC' => 'T', 'AAC' => 'N', 'AGC' => 'S',
+    'ATA' => 'I', 'ACA' => 'T', 'AAA' => 'K', 'AGA' => 'R',
+    'ATG' => 'M', 'ACG' => 'T', 'AAG' => 'K', 'AGG' => 'R',
+    'GTT' => 'V', 'GCT' => 'A', 'GAT' => 'D', 'GGT' => 'G',
+    'GTC' => 'V', 'GCC' => 'A', 'GAC' => 'D', 'GGC' => 'G',
+    'GTA' => 'V', 'GCA' => 'A', 'GAA' => 'E', 'GGA' => 'G',
+    'GTG' => 'V', 'GCG' => 'A', 'GAG' => 'E', 'GGG' => 'G',
+);
+
+ $count = 50;
+
+while ($count > 0) {
+    print "请输入三个DNA碱基（输入q退出）：";
+    $input = <STDIN>;
+    chomp $input;
+
+    if ($input eq 'q') {
+        last;
+    }
+
+    if (length($input) != 3) {
+        print "输入的DNA碱基必须为三个字符，请重新输入。\n";
+        next;
+    }
+
+     $codon = uc($input);
+    if (exists $codon_table{$codon}) {
+        $amino_acid = $codon_table{$codon};
+        print "三联体密码 $codon 对应的氨基酸为 $amino_acid\n";
+    } else {
+        print "无效的三联体密码，请重新输入。\n";
+    }
+
+    $count--;
+    print "剩余查询次数：$count\n";
+}
+
+print "程序结束。\n";
